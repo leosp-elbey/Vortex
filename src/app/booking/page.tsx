074ff065efc5101e2b1307629708ebbcc1d25427
VortexTrips <info@kelliworks.com>
2026-04-19 20:49:03 -0400

    Update booking URL

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 
 export default function BookingPage() {
   useEffect(() => {
-    window.location.replace('https://travmanity.com/Page/Home/wa=leosp?FpSubAffiliate=TM1705228')
+    window.location.replace('https://travmanity.com/Page/Home/wa=leosp?FpSubAffiliate')
   }, [])
 
   return (
