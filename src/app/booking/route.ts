fed4db3ad902a27da4ac997b77d50b1d52c0c87d
VortexTrips <info@kelliworks.com>
2026-04-19 20:40:02 -0400

    Remove conflicting booking route

--- src/app/booking/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export function GET() {
-  return NextResponse.redirect(
-    'https://travmanity.com/Page/Home/wa=leosp?FpSubAffiliate=TM1705228',
-    { status: 302 }
-  )
-}
